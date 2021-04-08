18d05e5b36ab0e65d2c9176ad37db733800d33c4
Jooonyub <59817144+Jooonyub@users.noreply.github.com>
2021-04-08 17:08:18 +0900

    KukkaApplication에 Timezone 업데이트

--- src/main/java/com/clone/kukka/controller/ProductController.java
+++ src/main/java/com/clone/kukka/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.clone.kukka.controller;
 
 import com.clone.kukka.entity.Comment;
 import com.clone.kukka.entity.Product;
+import com.clone.kukka.repository.CommentRepository;
 import com.clone.kukka.repository.ProductRepository;
 import com.clone.kukka.service.CommentService;
 import lombok.NoArgsConstructor;
@@ -18,18 +19,23 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository productRepository;
+//    private final CommentRepository commentRepository;
 
     @GetMapping("/api/products")
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
-//    Detail에 Product(1개) 뿌려줄 때, 해당하는 Comment들도 같이 뿌려주기
+//    //Detail에 Product(1개) 뿌려줄 때, 해당하는 Comment들도 같이 뿌려주기
 //    @GetMapping("/api/products/{productId}")
 //    public Product getProductDetail(@PathVariable Long productId) {
 //        return productRepository.findById(productId).orElseThrow(
 //                () -> new NullPointerException("해당하는 상품이 존재하지 않습니다.")
 //        );
+//        //해당 ProductId를 가진 comments를 불러온다.
+//        List<Comment> byProductIdOrderByModifiedAtDesc = commentRepository.findByProductIdOrderByModifiedAtDesc(productId);
+//        //return을 두개 내려주지 않는 한 안될것같은데?
+//
 //    }
 
 //    위에 합치기
